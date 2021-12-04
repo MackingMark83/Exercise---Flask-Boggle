d834bc18e55223c85df00a5215a8c08b07cfbae3
Marcus <marcusclemmons1@gmail.com>
2021-12-03 19:45:31 -0500

    made some new files

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from flask import Flask
+from flask import Flask, request, render_template, jsonify, session
 from boggle import Boggle
+app.config["SECRET_KEY"] = "mycode"
+
+app = Flask(__name__)
 
 
-app = Flask (__name__)
+boogle_game = Boggle()
 
-boogle_game = Boggle
+
+@app.route("/")
+def homepage():
+    """Show board."""
+
+    board = boggle_game.make_board()
+    session['board'] = board
+    highscore = session.get("highscore", 0)
+    nplays = session.get("nplays", 0)
+
+    return render_template("base.html", board=board,
+                           highscore=highscore,
+                           nplays=nplays)
