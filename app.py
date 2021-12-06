f5d03fdee9a0e69497989b5a0bf44b041b39d963
Marcus <marcusclemmons1@gmail.com>
2021-12-06 10:11:54 -0500

    new test

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from flask import Flask, request, render_template, jsonify, session
 from boggle import Boggle
-app.config["SECRET_KEY"] = "mycode"
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "fdfgkjtjkkg45yfdb"
 
-
-boogle_game = Boggle()
+boggle_game = Boggle()
 
 
 @app.route("/")
@@ -17,6 +16,31 @@ def homepage():
     highscore = session.get("highscore", 0)
     nplays = session.get("nplays", 0)
 
-    return render_template("base.html", board=board,
+    return render_template("index.html", board=board,
                            highscore=highscore,
                            nplays=nplays)
+
+
+@app.route("/check-word")
+def check_word():
+    """Check if word is in dictionary."""
+
+    word = request.args["word"]
+    board = session["board"]
+    response = boggle_game.check_valid_word(board, word)
+
+    return jsonify({'result': response})
+
+
+@app.route("/post-score", methods=["POST"])
+def post_score():
+    """Receive score, update nplays, update high score if appropriate."""
+
+    score = request.json["score"]
+    highscore = session.get("highscore", 0)
+    nplays = session.get("nplays", 0)
+
+    session['nplays'] = nplays + 1
+    session['highscore'] = max(score, highscore)
+
+    return jsonify(brokeRecord=score > highscore)
--- test.py
+++ test.py
@@ -8,3 +8,28 @@ class FlaskTests(TestCase):
 
     # TODO -- write tests for every view function / feature!
 
+def test_homepage(self):
+    with app.test_client() as client:
+        response = self.client.get("/")
+        html = res.get_data(as_text=Ture)
+
+        self.assertIn('<td>{{ cell }}</td>', html)
+
+
+
+
+
+def test_check_word(self):
+    with app.test_client() as client:
+        self.assertEqual((response.json['result'], 'ok')
+)
+
+
+
+
+def test_post_score(self):
+    with app.test_client() as client: 
+        
+        self.client.post(/'post-score',data = {'highscore': 0} )
+        self.assertEqual(res.status_code, 200)
+        self.assertIn('<input name='word'>', html)        
