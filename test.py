5afc3f664f7ce340fb27709b8e1392d9098c2578
Marcus <marcusclemmons1@gmail.com>
2021-12-06 14:14:09 -0500

    all done

--- test.py
+++ test.py
@@ -5,31 +5,35 @@ from boggle import Boggle
 
 
 class FlaskTests(TestCase):
+    
+        
+    def test_homepage(self):
+        with app.test_client() as client:
+            res = client.get("/")
+            html = res.get_data(as_text=True)
 
+            self.assertEqual(res.status_code, 200)
+            self.assertIn('<button>Enter</button>', html)
+    
+    def test_find_from(self):
+        with app.test_client() as client:
+            res = client.get("/")
+            html = res.get_data(as_text=True)
+
+    def test_find(self):
+        with app.test_client() as client:
+            res = client.get("/")
+            html = res.get_data(as_text=True)
+
+            self.assertNotIn('!<td>{{ H }}</td>', html)        
+
+
+    
+    
+
+
+
+
+    
     # TODO -- write tests for every view function / feature!
 
-def test_homepage(self):
-    with app.test_client() as client:
-        response = self.client.get("/")
-        html = res.get_data(as_text=Ture)
-
-        self.assertIn('<td>{{ cell }}</td>', html)
-
-
-
-
-
-def test_check_word(self):
-    with app.test_client() as client:
-        self.assertEqual((response.json['result'], 'ok')
-)
-
-
-
-
-def test_post_score(self):
-    with app.test_client() as client: 
-        
-        self.client.post(/'post-score',data = {'highscore': 0} )
-        self.assertEqual(res.status_code, 200)
-        self.assertIn('<input name='word'>', html)        
